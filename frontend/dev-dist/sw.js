3f5ee5c0ef5362bebe7112ebf419309a6206b2a9
Pedro Coelho <pedrohjc180@gmail.com>
2026-02-01 17:39:22 -0300

    Sync: Force push all local changes to production

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q04cf0mtha8"
+    "revision": "0.6718m5de0fg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
