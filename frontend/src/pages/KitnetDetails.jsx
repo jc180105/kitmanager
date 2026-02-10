f37bec988cb02667ec40c9be5383a3c6c38fd654
Pedro Coelho <pedrohjc180@gmail.com>
2026-02-10 00:45:36 -0300

    Fix missing import in KitnetDetails.jsx

--- frontend/src/pages/KitnetDetails.jsx
+++ frontend/src/pages/KitnetDetails.jsx
@@ -7,6 +7,7 @@ import {
 import { toast } from 'sonner';
 import { api } from '../utils/api';
 import { generateContract } from '../utils/generateContract';
+import PaymentConfirmDialog from '../components/PaymentConfirmDialog';
 
 export default function KitnetDetails() {
     const { id } = useParams();
